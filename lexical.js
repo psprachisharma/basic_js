343ac7e0c4bf10205ca7407192eb194a9d1fb77a
Prachi Sharma <prachiarsharma@gmail.com>
2021-08-17 08:43:49 +0530

    destructuring.js

---
+++ lexical.js
@@ -0,0 +1,83 @@
+Comments
+
+// This is a single-line comment.
+
+/* This is also a comment */ //and here is another comment
+
+/* 
+ *   This is a multi-line comment. The extra * characters at the start of
+ *   each line are not a required part of the syntax; they just look cool :) 
+ */
+
+ Literals 
+ 12 // the number twelve
+ 1.2 // the number one point two
+ "hello world" // a string of text
+'hi' // another string
+true // a boolean value
+false // the other boolean value
+null // absence of an object
+
+Identifiers and Reserved Words
+i
+my_variable_name
+v13
+_dummy
+$str
+
+Reserved Words
+as 
+async
+await
+break
+case
+catch
+class
+const 
+continue
+debugger
+default
+delete
+do
+else
+export
+extends
+false
+finally
+for
+from
+function
+get
+if 
+import
+in  
+instanceof
+let
+new 
+null 
+of 
+return
+set
+static
+super
+switch 
+target 
+this 
+throw 
+true 
+try
+typeof
+var
+void
+while
+with 
+yield 
+enum
+implements
+interface
+package
+private
+protected 
+
+// arguments and eval are not allowed as identifiers 
+// in certain circumstances and are best avoided entirely
