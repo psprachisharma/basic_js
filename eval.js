3fef6959302f776563b616026e357986ef2ae4f8
Prachi Sharma <prachiarsharma@gmail.com>
2021-08-18 14:47:08 +0530

    eval.js

--- eval.js
+++ eval.js
@@ -20,3 +20,16 @@ function g() { // this function does a global eval
 }
 console.log(f(), x); // Local variable changed: prints "localchanged global"
 console.log(g(), y); // Global variable changed: prints "local globalchanged"
+
+
+/* Strict eval()
+Strict mode imposes further restrictions on the behavior of the eval() function and even on 
+the use of the identifier “eval”. When eval() is called from strict-mode code, or when the 
+string of code to be evaluated itself begins with a “use strict” directive, then eval() does 
+a local eval with a private variable environment. This means that in strict mode, evaluated 
+code can query and set local variables, but it cannot define new variables or functions in the local scope.
+
+Furthermore, strict mode makes eval() even more operator-like by effectively making “eval” 
+into a reserved word. You are not allowed to overwrite the eval() function with a new value. 
+And you are not allowed to declare a variable, function, function parameter, or catch block 
+parameter with the name “eval”. */
