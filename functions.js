5a5c58b618c567c27ba004af70faa4f322f86d15
Prachi Sharma <prachiarsharma@gmail.com>
2021-08-25 14:10:52 +0530

    functions.js

--- functions.js
+++ functions.js
@@ -124,3 +124,91 @@ function sum(a) {
 sum([1,2,3])            // => 6
 sum(1,2,3);              // !TypeError: 1 is not iterable
 sum([1,2,"3"]);         // !TypeError: element 2 is not a number
+
+// Using functions as data
+// We define some simple functions here
+function add(x,y) { return x+y; }
+function subtract(x,y) { return x-y; }
+function multiply(x,y) { return x*y; }
+function divide(x,y) { return x/y; }
+
+/* Here's a function that takes one of the preceding functions as an argument and 
+invokes it on two operands */
+function operate(operator, operand1, operand2){
+    return operator(operand1, operand2);
+}
+
+// We could invoke this function like this to compute the value (2+3) + (4*5):
+let i = operate(add, operate(add, 2, 3), operate(multiply, 4, 5));
+
+/* For the sake of the example, we implement the simple functions again, 
+this time within an object literal; */
+const operators = {
+    add:        (x,y) => x+y,
+    subtract:   (x,y) => x-y,
+    multiply:   (x,y) => x*y,
+    divide:     (x,y) => x/y,
+    pow:        Math.pow // This works for predefined functions too
+};
+
+/* This function takes the name of an operator, looks up that operator in the Object, 
+and then invokes it on the supplied operands. Note the syntax used to invoke the
+operator function. */
+function operate2(operation, operand1, operand2){
+    if(typeof operators[operation] === "function") {
+        return operators[operation](operand1, operand2);
+    }
+    else throw "unknown operator";
+}
+
+operate2("add", "hello", operate2("add"," ","world")) //=> "hello world"
+operate2("pow", 10, 2) //=> 100
+
+// Defining your own function properties 
+/* Initialize the counter property of the function object.
+Function declarations are hoisted so we really can do this assignemnt before 
+the function declaration. */
+uniqueInteger.counter = 0;
+
+/* This function returns a different integer each time it is called. It uses a property 
+of itself to remember the next value to be returned.  */
+function uniqueInteger(){
+    return uniqueInteger.counter++; // return and increment counter property
+}
+uniqueInteger()     // => 0
+uniqueInteger()     // => 1
+
+// Compute factorials and cache results as properties of the function itself.
+function factorial(n){
+    if(Number.isInteger(n) && n > 0){           // Positive integers only
+        if(!(n in factorial)){                  // If no cached result
+            factorial[n] = n*factorial(n-1);    //Compute and cache it
+        }
+        return factorial[n];                    // Return the cached result
+    } else {
+        return NaN;                             // If inout was bad
+    }
+}
+factorial[1] = 1;          // Initialize the cache to hold this base case.
+factorial(6)               // => 720
+factorial[5]               // => 120; the call above caches this value
+
+function counter() {
+    let n = 0;
+    return {
+        count: function() { return n++;},
+        reset: function() { n = 0; }
+    };
+}
+let c = counter(), d = counter();   // Create two counters
+c.count()                           // => 0
+d.count()                           // => 0; they count independently
+c.reset()                           // reset() and count() methods share state
+c.count()                           // => 0: becaue we reset c
+d.count()                           // => 1: d was not reset
+
+function counter(n){
+    return {
+        // Property getter method returns and 
+    }
+}
